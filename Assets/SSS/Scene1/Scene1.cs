06db6e17c974119b14448dc88d9f9cd27fbe814c
Quốc Việt Đoàn <vietunitydev2222@gmail.com>
2024-01-05 19:16:58 +0700

    revert backup 4

--- Assets/SSS/Scene1/Scene1.cs
+++ Assets/SSS/Scene1/Scene1.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class Scene1 : SSController
 {
     int scene1 = 3;
-
-    int scene2 = 4;
     public void OnPopupButtonClick()
     {
         SSSceneManager.Instance.PopUp("Popup", "Nav Mesh",
@@ -27,10 +25,6 @@ public class Scene1 : SSController
         SSSceneManager.Instance.Screen("Navmesh");
         Debug.Log("OnPopupOkButtonTap2 ===");
         Debug.Log("OnPopupOkButtonTap");
-       
-        Debug.Log("OnPopupOkButtonTap3 ====");
-        Debug.Log("OnPopupOkButtonTap4 ==== test conflict 3");
-
     }
 
 }
